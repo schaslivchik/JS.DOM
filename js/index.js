4160f1b01d3d58d77cc9e04a021a527229fcbe80
Mikhail <schaslivchikm@gmail.com>
2022-02-08 21:40:20 +0200

    3 tasks done

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@ buttonChangeColor.onclick = () => {
 
 
 //3
+// 
+const imgChange = document.querySelector(".img");
+
+imgChange.addEventListener('mouseover', function(){
+  this.src = `https://farm2.staticflickr.com/1949/45717354341_a8dc471d63_b.jpg`
+})
+imgChange.addEventListener('mouseleave', function(){
+  this.src = `https://s3.nat-geo.ru/images/2020/9/13/d34f669d4d6e4dba824f17593319ca13.max-2500x1500.jpg`
+})
